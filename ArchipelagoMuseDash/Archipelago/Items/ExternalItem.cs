4f949615211ef65b65ed1098b6384ec0f79b41a7
Justus Lind <DeamonHunter@users.noreply.github.com>
2023-02-19 13:50:59 +1000

    Fix crash when displaying external item. Actually make it show the song if its a muse dash item. (Or if it has the same name as one. :P)

--- ArchipelagoMuseDash/Archipelago/Items/ExternalItem.cs
+++ ArchipelagoMuseDash/Archipelago/Items/ExternalItem.cs
@@ -1,21 +1,27 @@
 ﻿namespace ArchipelagoMuseDash.Archipelago.Items {
     public class ExternalItem : IMuseDashItem {
-        public string UnlockSongUid => ArchipelagoStatic.AlbumDatabase.GetMusicInfo("Magical Wonderland[Default Music]").uid;
-        public bool UseArchipelagoLogo => true;
+        public string UnlockSongUid => _songUID ?? ArchipelagoStatic.AlbumDatabase.GetMusicInfo("Magical Wonderland").uid;
+
+        public bool UseArchipelagoLogo => _songUID == null;
 
         public string TitleText => "New Item!!";
         public string SongText => _itemName;
         public string AuthorText => $"Sent to {_receivingPlayerName}.";
 
         public string PreUnlockBannerText => "A new item?";
-        public string PostUnlockBannerText => "Hope it's good!"; //Todo: See if we can get type info.
+        public string PostUnlockBannerText => _songUID != null ? "It ain't yours." : "Hope it's good!"; //Todo: See if we can get type info.
 
         readonly string _receivingPlayerName;
         readonly string _itemName;
 
+        readonly string _songUID;
+
         public ExternalItem(string itemName, string receivingPlayer) {
             _itemName = itemName.Replace('_', ' ');
             _receivingPlayerName = receivingPlayer;
+
+            if (ArchipelagoStatic.AlbumDatabase.TryGetMusicInfo(itemName, out var info))
+                _songUID = info.uid;
         }
 
         public void UnlockItem(ItemHandler handler, bool immediate) { }
