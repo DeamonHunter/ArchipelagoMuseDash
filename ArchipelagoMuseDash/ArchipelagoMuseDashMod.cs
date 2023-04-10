5f2709344862207cdb6a24b53a97691557ff67ca
Justus Lind <DeamonHunter@users.noreply.github.com>
2023-04-10 14:39:30 +1000

    Update build number.

--- ArchipelagoMuseDash/ArchipelagoMuseDashMod.cs
+++ ArchipelagoMuseDash/ArchipelagoMuseDashMod.cs
@@ -3,7 +3,7 @@ using ArchipelagoMuseDash.Archipelago;
 using ArchipelagoMuseDash.Helpers;
 using ArchipelagoMuseDash.Logging;
 using MelonLoader;
-[assembly: MelonInfo(typeof(ArchipelagoMuseDashMod), "Archipelago Muse Dash", "0.9.0", "DeamonHunter")]
+[assembly: MelonInfo(typeof(ArchipelagoMuseDashMod), "Archipelago Muse Dash", "0.9.1", "DeamonHunter")]
 [assembly: MelonGame("PeroPeroGames", "MuseDash")]
 
 namespace ArchipelagoMuseDash;
