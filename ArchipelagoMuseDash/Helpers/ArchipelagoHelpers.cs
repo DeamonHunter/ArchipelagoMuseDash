10c31cab3656dfaf96e66da1900d10b3a6f95753
Justus Lind <DeamonHunter@users.noreply.github.com>
2023-10-21 14:53:00 +1000

    Fix null ref on starting archipelago if you didn't go into the main menu.

--- ArchipelagoMuseDash/Helpers/ArchipelagoHelpers.cs
+++ ArchipelagoMuseDash/Helpers/ArchipelagoHelpers.cs
@@ -77,8 +77,10 @@ public static class ArchipelagoHelpers {
     public static void SetBackToDefaultFilter() {
         ArchipelagoStatic.ArchLogger.Log("Helpers", $"Tag Index: {GlobalDataBase.dbMusicTag.selectedTagIndex}");
         GlobalDataBase.dbMusicTag.selectedTagIndex = 1; // 0 is favourite songs, 1 is all songs
-        ArchipelagoStatic.SongSelectPanel.m_PnlMusicTag.SetTabIndex(0);
-        ArchipelagoStatic.SongSelectPanel.m_PnlMusicTag.RefreshAllShownItem();
+        if (ArchipelagoStatic.SongSelectPanel) {
+            ArchipelagoStatic.SongSelectPanel.m_PnlMusicTag.SetTabIndex(0);
+            ArchipelagoStatic.SongSelectPanel.m_PnlMusicTag.RefreshAllShownItem();
+        }
         MusicTagManager.instance.RefreshStageDisplayMusics();
     }
 }
