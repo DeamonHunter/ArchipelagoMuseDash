b72465ff8333b6034f6f2d2bb6858b6a3279ea1f
Justus Lind <deamonhunterextra@outlook.com>
2025-03-28 20:44:47 +1000

    Update to Muse Dash Legend. Increase Version

--- ArchipelagoMuseDash/ArchipelagoMuseDashMod.cs
+++ ArchipelagoMuseDash/ArchipelagoMuseDashMod.cs
@@ -4,7 +4,7 @@ using ArchipelagoMuseDash.Helpers;
 using ArchipelagoMuseDash.Logging;
 using MelonLoader;
 
-[assembly: MelonInfo(typeof(ArchipelagoMuseDashMod), "Archipelago Muse Dash", "1.5.12", "DeamonHunter")]
+[assembly: MelonInfo(typeof(ArchipelagoMuseDashMod), "Archipelago Muse Dash", "1.5.13", "DeamonHunter")]
 [assembly: MelonGame("PeroPeroGames", "MuseDash")]
 [assembly: MelonPriority(100)]
 
